98531cb2fd1abec06bdc551533f5671516b5ea47
Jesse Alltop <Jesse@macbook-pro-2.local>
2016-04-02 14:20:08 -0500

    geofence in place

--- PokemonRedbirdHacks/ViewController.swift
+++ PokemonRedbirdHacks/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 var stepCounter:Int = 0
 
@@ -28,8 +29,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
     
+    //CLCircularRegion region = new CLCircularRegion(new CLLocationCoordinate2D(+37.29430502,-122.0942
     
-  
+    
     
     
     override func viewDidLoad() {
@@ -41,23 +43,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
-               
+ 
+        //CLCircularRegion.initCircularRegionWithCenter([0, 0], radius: 100, identifier: "whatever")
+        
+      //  CLCircularRegion.init(center: CLLocationCoordinate2D.init(latitude: 40.81150,longitude: 14.42753), radius: 10, identifier: "whatever")
+    
+        locationManager.startMonitoringForRegion(CLCircularRegion.init(center: CLLocationCoordinate2D.init(latitude: 28.04681,longitude: 86.92858), radius: 10, identifier: "everest"))
         
         
+        locationManager.startMonitoringForRegion(CLCircularRegion.init(center: CLLocationCoordinate2D.init(latitude: 40.508644,longitude: -88.990932), radius: 10, identifier: "quad"))
+        
+        
+        locationManager.startMonitoringForRegion(CLCircularRegion.init(center: CLLocationCoordinate2D.init(latitude: 40.81900,longitude: 14.42753), radius: 10, identifier: "pompeii"))
 
         
-        
-        
-        
-        
-        
-        
-        
+        locationManager.startMonitoringForRegion(CLCircularRegion.init(center: CLLocationCoordinate2D.init(latitude: 35.66336,longitude: 139.66833), radius: 10, identifier: "japan"))
+
     }
     
     func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
-        print(stepCounter)
-        stepCounter += 1
         centerMapOnLocation(locationManager.location!)
     }
     
@@ -75,6 +79,47 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
 
 
+    
+    @IBAction func regionMonitoring(sender: AnyObject) {
+        locationManager.requestAlwaysAuthorization()
+        
+//        let currRegion = CLCircularRegion(center: CLLocationCoordinate2D(latitude: 40.81150, longitude: 14.42753), radius: 10, identifier: "Home")
+//        locationManager.startMonitoringForRegion(currRegion)
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didEnterRegion currRegion: CLRegion) {
+//        NSLog("Entering region")
+//        print(currRegion.identifier)
+
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion currRegion: CLRegion) {
+        NSLog("Exit region")
+        print(currRegion.identifier)
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        NSLog("\(error)")
+    }
+
+    
+    
+    
+    
+    //encounter system
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
+
 
 
